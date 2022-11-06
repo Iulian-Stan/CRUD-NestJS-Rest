f067f53149433f0888d0ff035dda10f462787328
Iulian-Stan <moldova.rules@yahoo.com>
2022-11-06 18:30:46 +0200

    Removed class-validator

--- src/users/model/user.entity.ts
+++ src/users/model/user.entity.ts
@@ -1,22 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { IsInt, IsString, IsOptional } from 'class-validator';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   @ApiProperty({ example: '1', description: 'Unique identifier', required: false })
-  @IsInt()
-  @IsOptional()
   id: number;
 
   @Column({ length: 50 })
   @ApiProperty({ example: 'Susie', description: 'User\'s first_name' })
-  @IsString()
   first_name: string;
 
   @Column({ length: 50 })
   @ApiProperty({ example: 'Osborne', description: 'User\'s last_name' })
-  @IsString()
   last_name: string;
 }
