d75be05d923b15ab2e5bd7d60489461e30098a56
Iulian-Stan <moldova.rules@yahoo.com>
2022-12-01 18:21:06 +0200

    Updated swagger responses info.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,7 +12,7 @@ export class UsersController {
 
   @Post()
   @ApiOperation({ summary: 'Create user' })
-  @ApiResponse({ status: 201, description: 'User created', type: User })
+  @ApiResponse({ status: 201, description: 'Created user data', type: User })
   create(@Body() user: UserDto): Promise<User> {
     return this.usersService.create(user);
   }
@@ -26,32 +26,28 @@ export class UsersController {
 
   @Get(':email')
   @ApiOperation({ summary: 'Find user by email' })
-  @ApiResponse({ status: 200, description: 'User found', type: User })
-  @ApiResponse({ status: 404, description: 'User not found' })
-  findOne(@Param('email') email: string): Promise<User> {
+  @ApiResponse({ status: 200, description: 'User data if user is found', type: User })
+  async findOne(@Param('email') email: string): Promise<User> {
     return this.usersService.findOne(email);
   }
 
   @Delete(':email')
   @ApiOperation({ summary: 'Delete user' })
-  @ApiResponse({ status: 204, description: 'User deleted' })
-  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 200, description: 'User was deleted if found' })
   remove(@Param('email') email: string): Promise<void> {
     return this.usersService.remove(email);
   }
 
   @Put(':email')
   @ApiOperation({ summary: 'Update user' })
-  @ApiResponse({ status: 200, description: 'User updated', type: User })
-  @ApiResponse({ status: 201, description: 'User created', type: User })
+  @ApiResponse({ status: 200, description: 'Updated user data if user is found', type: User })
   update(@Param('email') email: string, @Body() user: UserDto): Promise<User> {
     return this.usersService.update(email, user);
   }
 
   @Get('books/:email')
   @ApiOperation({ summary: 'List user\'s books' })
-  @ApiResponse({ status: 200, description: 'User found', type: [Book] })
-  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 200, description: 'List of books if user is found', type: [Book] })
   getBooks(@Param('email') email: string) {
     return this.usersService.getBooks(email);
   }
